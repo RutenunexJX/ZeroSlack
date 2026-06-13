dde5933c8b465d5e2df2f7880108f6727bd71197
RxJX <xqj1497177901@gmail.com>
2026-06-13 23:24:13 +0800

    Extract workspace symbol lifecycle

--- workspacesymbolanalysiscontroller.cpp
+++ workspacesymbolanalysiscontroller.cpp
@@ -1,6 +1,5 @@
 #include "workspacesymbolanalysiscontroller.h"
 
-#include "semanticindex.h"
 #include "symbolanalyzer.h"
 
 WorkspaceSymbolAnalysisController::WorkspaceSymbolAnalysisController(QObject* parent)
@@ -73,63 +72,4 @@ void WorkspaceSymbolAnalysisController::setCancelProvider(
     std::function<bool()> provider)
 {
     cancelProvider = std::move(provider);
-}
-
-void WorkspaceSymbolAnalysisController::requestWorkspaceAnalysis(
-    const ProjectSnapshot& project)
-{
-    if (!project.isOpen() || !symbolAnalyzer)
-        return;
-
-    SemanticIndex::getInstance()->clearSnapshot();
-    if (project.systemVerilogFiles.isEmpty())
-        return;
-
-    activeProject = project;
-    workspaceAnalysisActive = true;
-    emit diagnosticsRefreshRequested(QString());
-    emit workspaceSymbolAnalysisStarted(project, project.systemVerilogFiles.size());
-    symbolAnalyzer->startAnalyzeProjectAsync(project, cancelProvider);
-}
-
-void WorkspaceSymbolAnalysisController::clearProjectSemanticState()
-{
-    if (projectSemanticStateCleared)
-        return;
-
-    projectSemanticStateCleared = true;
-    workspaceAnalysisActive = false;
-    activeProject = ProjectSnapshot();
-    emit workspaceRelationshipAnalysisCancelRequested();
-    SemanticIndex::getInstance()->clearSnapshot();
-    emit relationshipDataClearRequested();
-    emit diagnosticsRefreshRequested(QString());
-}
-
-void WorkspaceSymbolAnalysisController::onProjectChanged(
-    const ProjectSnapshot& project)
-{
-    if (!project.isOpen()) {
-        clearProjectSemanticState();
-        return;
-    }
-
-    projectSemanticStateCleared = false;
-    requestWorkspaceAnalysis(project);
-}
-
-void WorkspaceSymbolAnalysisController::onWorkspaceSymbolAnalysisCompleted(
-    int filesAnalyzed,
-    int totalSymbols)
-{
-    if (!workspaceAnalysisActive)
-        return;
-
-    workspaceAnalysisActive = false;
-    const ProjectSnapshot project = activeProject;
-    if (cancelProvider && cancelProvider())
-        return;
-
-    emit workspaceSymbolAnalysisFinished(project, filesAnalyzed, totalSymbols);
-    emit workspaceRelationshipAnalysisRequested(project);
 }
---
+++ workspacesymbolanalysiscontrollerlifecycle.cpp
@@ -0,0 +1,63 @@
+#include "workspacesymbolanalysiscontroller.h"
+
+#include "semanticindex.h"
+#include "symbolanalyzer.h"
+
+void WorkspaceSymbolAnalysisController::requestWorkspaceAnalysis(
+    const ProjectSnapshot& project)
+{
+    if (!project.isOpen() || !symbolAnalyzer)
+        return;
+
+    SemanticIndex::getInstance()->clearSnapshot();
+    if (project.systemVerilogFiles.isEmpty())
+        return;
+
+    activeProject = project;
+    workspaceAnalysisActive = true;
+    emit diagnosticsRefreshRequested(QString());
+    emit workspaceSymbolAnalysisStarted(project, project.systemVerilogFiles.size());
+    symbolAnalyzer->startAnalyzeProjectAsync(project, cancelProvider);
+}
+
+void WorkspaceSymbolAnalysisController::clearProjectSemanticState()
+{
+    if (projectSemanticStateCleared)
+        return;
+
+    projectSemanticStateCleared = true;
+    workspaceAnalysisActive = false;
+    activeProject = ProjectSnapshot();
+    emit workspaceRelationshipAnalysisCancelRequested();
+    SemanticIndex::getInstance()->clearSnapshot();
+    emit relationshipDataClearRequested();
+    emit diagnosticsRefreshRequested(QString());
+}
+
+void WorkspaceSymbolAnalysisController::onProjectChanged(
+    const ProjectSnapshot& project)
+{
+    if (!project.isOpen()) {
+        clearProjectSemanticState();
+        return;
+    }
+
+    projectSemanticStateCleared = false;
+    requestWorkspaceAnalysis(project);
+}
+
+void WorkspaceSymbolAnalysisController::onWorkspaceSymbolAnalysisCompleted(
+    int filesAnalyzed,
+    int totalSymbols)
+{
+    if (!workspaceAnalysisActive)
+        return;
+
+    workspaceAnalysisActive = false;
+    const ProjectSnapshot project = activeProject;
+    if (cancelProvider && cancelProvider())
+        return;
+
+    emit workspaceSymbolAnalysisFinished(project, filesAnalyzed, totalSymbols);
+    emit workspaceRelationshipAnalysisRequested(project);
+}
