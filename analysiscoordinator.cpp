d10145c8877fc64758a2d6b8d8d973e46f512287
RxJX <xqj1497177901@gmail.com>
2026-06-13 20:43:20 +0800

    Extract analysis coordinator wiring

--- analysiscoordinator.cpp
+++ analysiscoordinator.cpp
@@ -8,7 +8,6 @@
 #include "tabmanager.h"
 #include "workspacemanager.h"
 
-#include <QFileInfo>
 #include <QTimer>
 
 #include <utility>
@@ -209,93 +208,6 @@ void AnalysisCoordinator::configureScheduler()
     });
 }
 
-void AnalysisCoordinator::connectSchedulerSignals()
-{
-    if (!dependencies.hasScheduler())
-        return;
-
-    AnalysisScheduler* scheduler = dependencies.schedulerObject();
-    connect(scheduler,
-            &AnalysisScheduler::relationshipDataRefreshRequested,
-            this, [this]() {
-                dependencies.refreshRelationshipDataView();
-            });
-    connect(scheduler,
-            &AnalysisScheduler::fileSymbolAnalysisFinished,
-            this, [this](const QString& fileName, int symbolCount) {
-                dependencies.handleFileSymbolAnalysisFinished(fileName,
-                                                              symbolCount);
-                refreshActiveEditorForFile(fileName);
-            });
-    connect(scheduler,
-            &AnalysisScheduler::workspaceSymbolAnalysisProgress,
-            this,
-            [this](const QString& fileName, int filesDone, int totalFiles) {
-                dependencies.handleWorkspaceSymbolProgress(fileName,
-                                                           filesDone,
-                                                           totalFiles);
-            });
-    connect(scheduler,
-            &AnalysisScheduler::workspaceSymbolAnalysisFinished,
-            this,
-            [this](const ProjectSnapshot&, int filesAnalyzed, int totalSymbols) {
-                dependencies.handleWorkspaceSymbolAnalysisFinished(
-                    filesAnalyzed,
-                    totalSymbols);
-            });
-    connect(scheduler,
-            &AnalysisScheduler::relationshipAnalysisFinished,
-            this, [this](const SingleFileRelationshipAnalysisResult& result) {
-                showRelationshipAnalysisCompleted(result.fileName,
-                                                  result.relationships.size());
-            });
-    connect(scheduler,
-            &AnalysisScheduler::documentRefreshRequested,
-            this, &AnalysisCoordinator::refreshActiveEditorForFile);
-    connect(scheduler,
-            &AnalysisScheduler::diagnosticsRefreshRequested,
-            this, [this](const QString& fileName) {
-                if (problemsRefreshHandler)
-                    problemsRefreshHandler(fileName);
-            });
-}
-
-void AnalysisCoordinator::connectProgressSignals()
-{
-    if (!dependencies.hasProgressCoordinator())
-        return;
-
-    dependencies.connectProgressToScheduler();
-    AnalysisProgressCoordinator* progressCoordinator =
-        dependencies.progressCoordinatorObject();
-    connect(progressCoordinator,
-            &AnalysisProgressCoordinator::statusMessageRequested,
-            this,
-            [this](const QString& message, int timeoutMs) {
-                if (statusMessageHandler)
-                    statusMessageHandler(message, timeoutMs);
-            });
-    connect(progressCoordinator,
-            &AnalysisProgressCoordinator::relationshipAnalysisErrorReported,
-            this,
-            [this](const QString&, const QString& error) {
-                showRelationshipAnalysisError(error);
-            });
-}
-
-void AnalysisCoordinator::connectWorkspaceSignals()
-{
-    if (!dependencies.hasWorkspaceFileWatcher())
-        return;
-
-    connect(dependencies.workspaceManagerObject(),
-            &WorkspaceManager::fileChanged,
-            this, [this](const QString& filePath) {
-                dependencies.handleExternalFileChanged(filePath,
-                                                       fileChangeDebounceMs);
-            });
-}
-
 void AnalysisCoordinator::refreshActiveEditorForFile(const QString& fileName) const
 {
     MyCodeEditor* editor = dependencies.currentEditor();
@@ -307,24 +219,4 @@ void AnalysisCoordinator::refreshActiveEditorForFile(const QString& fileName) co
     QTimer::singleShot(0, editor, [editor]() {
         editor->refreshScopeAndCurrentLineHighlight();
     });
-}
-
-void AnalysisCoordinator::showRelationshipAnalysisCompleted(
-    const QString& fileName,
-    int relationshipsFound) const
-{
-    if (!statusMessageHandler)
-        return;
-
-    statusMessageHandler(
-        QString("Smart analysis completed: %1 relationships in %2")
-            .arg(relationshipsFound)
-            .arg(QFileInfo(fileName).fileName()),
-        2000);
-}
-
-void AnalysisCoordinator::showRelationshipAnalysisError(const QString& error) const
-{
-    if (statusMessageHandler)
-        statusMessageHandler(QString("Analysis error: %1").arg(error), 3000);
 }
