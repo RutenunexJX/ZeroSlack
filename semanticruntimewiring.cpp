233883023dc4a40d0614fc6848bcc65914042181
RxJX <xqj1497177901@gmail.com>
2026-06-13 22:24:18 +0800

    Extract semantic runtime wiring

---
+++ semanticruntimewiring.cpp
@@ -0,0 +1,42 @@
+#include "semanticruntimecoordinator.h"
+
+#include "analysisscheduler.h"
+#include "completionservice.h"
+#include "definitionnavigationservice.h"
+#include "definitionservice.h"
+#include "diagnosticservice.h"
+#include "hierarchyservice.h"
+#include "navigationservice.h"
+#include "referenceservice.h"
+#include "relationshipservice.h"
+#include "searchservice.h"
+#include "scopebandservice.h"
+#include "semanticindex.h"
+#include "smartrelationshipbuilder.h"
+#include "symbolanalyzer.h"
+#include "symbolrelationshipengine.h"
+
+void SemanticRuntimeCoordinator::configureScheduler(AnalysisScheduler* scheduler) const
+{
+    if (!scheduler)
+        return;
+
+    scheduler->setSymbolAnalyzer(symbolAnalyzerInstance.get());
+    scheduler->setRelationshipEngine(relationshipEngineInstance.get());
+    scheduler->setRelationshipBuilder(relationshipBuilderInstance.get());
+}
+
+void SemanticRuntimeCoordinator::configureQueryServices(
+    SemanticIndex* semanticIndex) const
+{
+    CompletionService::getInstance()->setSemanticIndex(semanticIndex);
+    DefinitionService::getInstance()->setSemanticIndex(semanticIndex);
+    DefinitionNavigationService::getInstance()->setSemanticIndex(semanticIndex);
+    DiagnosticService::getInstance()->setSemanticIndex(semanticIndex);
+    RelationshipService::getInstance()->setSemanticIndex(semanticIndex);
+    ReferenceService::getInstance()->setSemanticIndex(semanticIndex);
+    HierarchyService::getInstance()->setSemanticIndex(semanticIndex);
+    SearchService::getInstance()->setSemanticIndex(semanticIndex);
+    ScopeBandService::getInstance()->setSemanticIndex(semanticIndex);
+    NavigationService::getInstance()->setSemanticIndex(semanticIndex);
+}
