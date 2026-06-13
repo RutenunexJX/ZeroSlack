c5b655e8b0fbc9adb6756b11f06793583fd0957a
RxJX <xqj1497177901@gmail.com>
2026-06-13 22:15:24 +0800

    Extract analysis coordinator dependencies

---
+++ analysiscoordinatordependencies.cpp
@@ -0,0 +1,142 @@
+#include "analysiscoordinator.h"
+
+#include "analysisprogresscoordinator.h"
+#include "analysisscheduler.h"
+#include "documentsnapshot.h"
+#include "navigationmanager.h"
+#include "semanticruntimecoordinator.h"
+#include "tabmanager.h"
+#include "workspacemanager.h"
+
+void AnalysisCoordinator::AnalysisDependencies::set(
+    AnalysisScheduler* newScheduler,
+    AnalysisProgressCoordinator* newProgressCoordinator,
+    SemanticRuntimeCoordinator* newSemanticRuntime,
+    TabManager* newTabManager,
+    WorkspaceManager* newWorkspaceManager,
+    NavigationManager* newNavigationManager)
+{
+    scheduler = newScheduler;
+    progressCoordinator = newProgressCoordinator;
+    semanticRuntime = newSemanticRuntime;
+    tabManager = newTabManager;
+    workspaceManager = newWorkspaceManager;
+    navigationManager = newNavigationManager;
+}
+
+bool AnalysisCoordinator::AnalysisDependencies::hasScheduler() const
+{
+    return scheduler != nullptr;
+}
+
+bool AnalysisCoordinator::AnalysisDependencies::hasProgressCoordinator() const
+{
+    return progressCoordinator != nullptr;
+}
+
+bool AnalysisCoordinator::AnalysisDependencies::hasWorkspaceFileWatcher() const
+{
+    return workspaceManager && scheduler;
+}
+
+void AnalysisCoordinator::AnalysisDependencies::configureScheduler() const
+{
+    if (!scheduler)
+        return;
+
+    scheduler->setDocumentModel(tabManager ? tabManager->getDocumentModel() : nullptr);
+    scheduler->setProjectModel(workspaceManager ? workspaceManager->getProjectModel() : nullptr);
+    if (semanticRuntime)
+        semanticRuntime->configureScheduler(scheduler);
+}
+
+void AnalysisCoordinator::AnalysisDependencies::connectProgressToScheduler() const
+{
+    if (progressCoordinator)
+        progressCoordinator->connectToScheduler(scheduler);
+}
+
+bool AnalysisCoordinator::AnalysisDependencies::isWorkspaceOpen() const
+{
+    return workspaceManager && workspaceManager->isWorkspaceOpen();
+}
+
+bool AnalysisCoordinator::AnalysisDependencies::isWorkspaceSymbolAnalysisCancelled() const
+{
+    return progressCoordinator
+        && progressCoordinator->isSymbolAnalysisCancelled();
+}
+
+void AnalysisCoordinator::AnalysisDependencies::refreshRelationshipDataView() const
+{
+    if (navigationManager)
+        navigationManager->refreshCurrentView();
+}
+
+void AnalysisCoordinator::AnalysisDependencies::handleFileSymbolAnalysisFinished(
+    const QString& fileName,
+    int symbolCount) const
+{
+    if (navigationManager)
+        navigationManager->onSymbolAnalysisCompleted(fileName, symbolCount);
+}
+
+void AnalysisCoordinator::AnalysisDependencies::handleWorkspaceSymbolProgress(
+    const QString& fileName,
+    int filesDone,
+    int totalFiles) const
+{
+    if (progressCoordinator) {
+        progressCoordinator->handleWorkspaceSymbolProgress(
+            filesDone,
+            totalFiles,
+            fileName);
+    }
+}
+
+void AnalysisCoordinator::AnalysisDependencies::handleWorkspaceSymbolAnalysisFinished(
+    int filesAnalyzed,
+    int totalSymbols) const
+{
+    if (navigationManager) {
+        navigationManager->onBatchSymbolAnalysisCompleted(
+            filesAnalyzed,
+            totalSymbols);
+    }
+}
+
+void AnalysisCoordinator::AnalysisDependencies::handleExternalFileChanged(
+    const QString& filePath,
+    int debounceMs) const
+{
+    if (scheduler)
+        scheduler->handleExternalFileChanged(filePath, debounceMs);
+}
+
+MyCodeEditor* AnalysisCoordinator::AnalysisDependencies::currentEditor() const
+{
+    return tabManager ? tabManager->getCurrentEditor() : nullptr;
+}
+
+DocumentSnapshot AnalysisCoordinator::AnalysisDependencies::currentDocument() const
+{
+    return tabManager ? tabManager->getCurrentDocument() : DocumentSnapshot();
+}
+
+AnalysisScheduler*
+AnalysisCoordinator::AnalysisDependencies::schedulerObject() const
+{
+    return scheduler;
+}
+
+AnalysisProgressCoordinator*
+AnalysisCoordinator::AnalysisDependencies::progressCoordinatorObject() const
+{
+    return progressCoordinator;
+}
+
+WorkspaceManager*
+AnalysisCoordinator::AnalysisDependencies::workspaceManagerObject() const
+{
+    return workspaceManager;
+}
